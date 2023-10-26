34a5909f2783e59bf86ad997cb21c9e838ec0ff8
EvenH0pe <149068214+EvenH0pe@users.noreply.github.com>
2023-10-26 15:34:39 +0300

    Praktika 4

---
+++ Praktika4.py
@@ -0,0 +1,31 @@
+import random
+print("\\\\\\\\\\\Да начнётся игра в камни!///////")
+
+def igra(n):
+    while n > 0:
+        print("Осталось камней:", n)
+        a=input("Выберите количество камней от 1 до 3: ")
+        try:
+            a=int(a)
+        except ValueError:
+            print("Вы должны ввести целое число камней")
+            exit(0)
+        if a>3 or a<1:
+            print("Должно быть введено значение от 1 до 3")
+            exit(0)
+        n -= a
+        if n <= 0:
+            print("Победа!")
+            exit(0)
+        comp=random.randint(1,3)
+        if comp==1:
+            print(f"Противник забрал {comp} камень")
+        else:
+            print(f"Противник забрал {comp} камня")
+        n-=comp
+        if n <= comp:
+            print("Вы проиграли")
+            exit(0)
+
+n = random.randint(4, 30)
+igra(n)
